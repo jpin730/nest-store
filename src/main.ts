463904d1d7e70d74de7c5e63fd32e4b6c8f928ce
Jaime Pineda <jpin730@pm.me>
2023-12-24 18:21:16 -0600

    refactor: :truck: move docs to root endpoint

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('', app, document);
 
   app.enableCors();
 
