9428508a51290a4ce854e75fbd3c26fc4af7c9b9
Jaime Pineda <jpin730@pm.me>
2024-01-21 16:38:07 -0600

    refactor: :fire: remove const product array in service

--- src/products/services/products.service.ts
+++ src/products/services/products.service.ts
@@ -8,17 +8,6 @@ import { Product } from '../entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  private products: Product[] = [
-    {
-      id: crypto.randomUUID(),
-      name: 'Product 1',
-      description: 'bla bla',
-      price: 122,
-      image: '',
-      stock: 12,
-    },
-  ];
-
   constructor(
     @InjectRepository(Product) private productRepo: Repository<Product>,
   ) {}
